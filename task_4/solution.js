9740c37b1983f30cdf0a38ec89fca609318394aa
lubavaiva <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>
2022-07-10 19:13:14 +0300

    Итоговое задание

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,18 +1,18 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
-    // Аргументом функции является JSON
-
-    // Преобразуйте строку json, переданную как аргумент функции,
-    // в объект с помощью функции JSON.parse(json)
-    // и запишите в переменную data
-    // Верните как результат функции свойство products объекта data
+   let data = JSON.parse(json);//  Присвойте переменной data результат преобразования аргумента в объект - JSON.parse(json)
+   let products = data.products;
+   return products; // Верните как результат функции свойство products объекта data
 }
 
-// Напишите функцию renderProductsCards(json)
-// Аргументом функции является JSON
-// Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
-// Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-// Запишите в переменную length значение свойства products.length
-// Напишите цикл, в котором перебираете все products от 0 до (length - 1)
-// и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+function renderProductsCards(json) {// Напишите функцию renderProductsCards(json)
+    
+    clearProducts(); // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
+    
+    let products = parseProducts(json);
+     
+    for  (let i = 0; i < products.length; i++) {
+    addProduct(products[i]);
+    } // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
+}
--- task_final/solution.js
+++ task_final/solution.js
@@ -5,7 +5,9 @@
 // - объект с адресом доставки: {street, house, entrance, floor, flat}
 // - список товаров в заказе
 // - стоимость заказа с учетом скидок и доставки
+
 // Как результат функции требуется вернуть JSON,
+
 // cформированный в соответствии с правилами:
 // Объект data содержит все данные
 // В объекте data есть свойства:
@@ -33,21 +35,16 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+function sendRequest(name, phone, address = {street, house, entrance, floor, flat}, goods, sum) {
+    let data = {client: name + ` ` + phone, order: {address: `ул. ` + address.street + `, дом ` + address.house +  `, ` + address.entrance +  ` подъезд, ` + address.floor +  ` этаж, кв ` + address.flat, sum}, goods:[]};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+    data.goods.push({title: goods[i].title, count: goods[i].count});
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    let jsonData = JSON.stringify({data});
+    
     return jsonData;
 }
+console.log (jsonData);
